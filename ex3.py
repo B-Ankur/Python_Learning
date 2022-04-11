7f6841c033c1af501d5b60a348145ea1d3121fea
B-Ankur <103422054+B-Ankur@users.noreply.github.com>
2022-04-11 19:05:48 +0530

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,5 +1,8 @@
+# Print the content inside double inverted as it is 
 print("I will now count my chickens: ")
 
+# Print Hens and calculate the maths acc. to BODMAS/PEDMAS rule
+# PE(MD)(AS)
 print("Hens", 25 + 30 / 6)
 print("Roosters", 100 - 25 * 3 % 4)
 
@@ -7,19 +10,22 @@ print("Now I will count the eggs: ")
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 print("Is it true that 3 + 2 < 5 -7?")
 
+# Print the output after calculating the maths 
 print(3 + 2 < 5 - 7)
 
 print("What is 3 + 2?", 3 + 2)
 print("What is 5 - 7?", 5 - 7)
 
+# Print the content inside double inverted as it is 
 print("Oh, that's why it's False.")
 
+# Print the content inside double inverted as it is 
 print("How about some more.")
 
+# the output will be in boolean i.e. true or false
 print("Is it greater?", 5 > -2)
 print("Is it greater or equal?", 5 >= -2)
 print("Is it less or equal?", 5 <= -2)
-
 
 
 
