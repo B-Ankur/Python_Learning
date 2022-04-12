81fbec5efd8d2d351134bc08c8b85f770881eb79
B-Ankur <103422054+B-Ankur@users.noreply.github.com>
2022-04-12 18:11:54 +0530

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,12 @@
+### ex9 printing, printing, printing ###
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+print("""
+There's something going here.
+With the three double-qoutes.
+Even 4 lines if we want, or 5, or 6
+""")
