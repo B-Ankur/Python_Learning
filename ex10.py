e9e6f32736ce8e589e724dcd85a0043826153a91
B-Ankur <103422054+B-Ankur@users.noreply.github.com>
2022-04-13 21:09:09 +0530

    Create ex10.py

---
+++ ex10.py
@@ -0,0 +1,17 @@
+### ex10 printing, printing, printing ###
+
+tabyy_cat = "\t I'm tabbed in."
+persian_cat = "I'm split\non a line."
+backlash_cat = "I'm \\ a \\cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabyy_cat)
+print(persian_cat)
+print(backlash_cat)
+print(fat_cat)
