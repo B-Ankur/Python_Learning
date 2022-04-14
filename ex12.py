79c8ca34859568ace9aca2898a507cd658176129
B-Ankur <103422054+B-Ankur@users.noreply.github.com>
2022-04-14 09:39:02 +0530

    Create ex12.py

---
+++ ex12.py
@@ -0,0 +1,7 @@
+### ex12 prompting people ###
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How do you weight? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
