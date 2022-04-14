f131689c788ce01f85f4fa55b832e1ea1e979857
B-Ankur <103422054+B-Ankur@users.noreply.github.com>
2022-04-14 09:23:25 +0530

    Create ex11.py

---
+++ ex11.py
@@ -0,0 +1,24 @@
+### ex11 printing, printing, printing ###
+
+print("How old are you?", end= '')
+age = input()
+print("How tall are you?", end= '')
+height = input()
+print("How do you weight?", end= '')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+
+
+### ex11 printing, printing, printing ###
+###
+print("How old are you?")
+age = input()
+print("How tall are you?")
+height = input()
+print("How do you weight?")
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+###
